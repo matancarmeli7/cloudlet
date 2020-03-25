92a71a001ce245f98043dfbb473111ad1851ad26
root <root@linux.l1ysxtozbfbunaeeh2nejct0wc.ax.internal.cloudapp.net>
2020-03-25 14:18:29 +0000

    Add image pull from central cloud

--- Salty/Salty.py
+++ Salty/Salty.py
@@ -1,37 +1,64 @@
 import requests
 import json
 
-# Set remote Quay registry with basic api prefix
-BASEURL = "https://quay.io/api/v1"
+def main():
 
-# Get all repositories
-URL = "{}/repository".format(BASEURL)
-PAR = {
-       'public':'true',
-       'starred':'false'
-      }
+	# List of repositories to pull from central cloud
+        repositories_to_pull = {}
 
-r = requests.get(url = URL, params = PAR, verify=False)
-response = r.json()
+	# Set local and remote Quay registries with basic api prefix
+	local_reg_URL = "https://quay.io/api/v1"
+        remote_req_URL = "https://quay.io/api/v1"
 
-data = json.loads(json.dumps(response))
+	# Get all repositories
+	URL = "{}/repository".format(local_reg_URL)
+	PAR = {
+	       'public':'true',
+	       'starred':'false'
+	      }
 
-# Go over each repository
-for repository in data['repositories']:
-    # Check if the repository is a Container Image Repository
-    if repository['kind'] == 'image':
+	r = requests.get(url = URL, params = PAR, verify=False)
+	response = r.json()
 
-        # Get more infromation on each repository
-        URL = "{}/repository/{}/{}".format(BASEURL, repository['namespace'], repository['name'])
+	data = json.loads(json.dumps(response))
 
-        r = requests.get(url = URL, verify=False)
-        response = r.json()
+	# Go over each repository
+	for repository in data['repositories']:
+	    # Check if the repository is a Container Image Repository
+	    if repository['kind'] == 'image':
 
-        data = json.loads(json.dumps(response))
+                # Get more infromation on each repository
+		URL = "{}/repository/{}/{}".format(local_reg_URL, repository['namespace'], repository['name'])
 
-        # Go over each tag in the repository and print each one with its full path and digest
-        for tag in data['tags'].keys():
-            print "{}/{}:{} {}".format(repository['namespace'], repository['name'], data['tags'][tag]['name'], data['tags'][tag]['manifest_digest'])
+		r = requests.get(url = URL, verify=False)
+		response = r.json()
 
-    else:
-        pass # Helm Charts here
+		data = json.loads(json.dumps(response))
+
+		# Go over each tag in the repository and print each one with its full path and digest
+		for tag in data['tags'].keys():
+		    print "{}/{}:{} {}".format(repository['namespace'], repository['name'], data['tags'][tag]['name'], data['tags'][tag]['manifest_digest'])
+
+                # Remember to pull this repository from the central cloud by namespace and repo name
+                repositories_to_pull.add("/{}/{}".format(repository['namespace'], repository['name']))
+
+	    else:
+		pass # Helm Charts here
+
+        # Get required repositories from central cloud
+        for repository in repositories_to_pull:
+
+             URL = "{}/repository/{}/{}".format(remote_reg_URL, repository['namespace'], repository['name'])
+          
+             r = requests.get(url = URL, verify=False)
+             response = r.json()
+
+             data = json.loads(json.dumps(response))
+
+             # Go over each tag in the repository and print each one with its full path and digest
+             for tag in data['tags'].keys():
+                 print "{}/{}:{} {}".format(repository['namespace'], repository['name'], data['tags'][tag]['name'], data['tags'][tag]['manifest_digest'])
+            
+
+if __name__ == "__main__":
+    main()
