de0d95408b9d84dfe3222279f4b5e737ff823cec
root <root@linux.l1ysxtozbfbunaeeh2nejct0wc.ax.internal.cloudapp.net>
2020-03-25 14:56:36 +0000

    Fix pulling of images from central cloud

--- Salty/Salty.py
+++ Salty/Salty.py
@@ -4,11 +4,11 @@ import json
 def main():
 
 	# List of repositories to pull from central cloud
-        repositories_to_pull = {}
+        repositories_to_pull = {None}
 
 	# Set local and remote Quay registries with basic api prefix
 	local_reg_URL = "https://quay.io/api/v1"
-        remote_req_URL = "https://quay.io/api/v1"
+        remote_reg_URL = "https://quay.io/api/v1"
 
 	# Get all repositories
 	URL = "{}/repository".format(local_reg_URL)
@@ -40,7 +40,7 @@ def main():
 		    print "{}/{}:{} {}".format(repository['namespace'], repository['name'], data['tags'][tag]['name'], data['tags'][tag]['manifest_digest'])
 
                 # Remember to pull this repository from the central cloud by namespace and repo name
-                repositories_to_pull.add("/{}/{}".format(repository['namespace'], repository['name']))
+                repositories_to_pull.add("{}/{}".format(repository['namespace'], repository['name']))
 
 	    else:
 		pass # Helm Charts here
@@ -48,7 +48,7 @@ def main():
         # Get required repositories from central cloud
         for repository in repositories_to_pull:
 
-             URL = "{}/repository/{}/{}".format(remote_reg_URL, repository['namespace'], repository['name'])
+             URL = "{}/repository/{}".format(remote_reg_URL, repository)
           
              r = requests.get(url = URL, verify=False)
              response = r.json()
@@ -57,7 +57,7 @@ def main():
 
              # Go over each tag in the repository and print each one with its full path and digest
              for tag in data['tags'].keys():
-                 print "{}/{}:{} {}".format(repository['namespace'], repository['name'], data['tags'][tag]['name'], data['tags'][tag]['manifest_digest'])
+                 print "{}:{} {}".format(repository, data['tags'][tag]['name'], data['tags'][tag]['manifest_digest'])
             
 
 if __name__ == "__main__":
