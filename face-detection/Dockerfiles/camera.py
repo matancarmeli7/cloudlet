38db0ed5a4e3a1e39c2fccaed13bb95846dafcb5
root <root@localhost.localdomain>
2020-05-15 05:34:16 +0300

    add face-detection

---
+++ face-detection/Dockerfiles/camera.py
@@ -0,0 +1,27 @@
+#camera.py
+# import the necessary packages
+import cv2
+# defining face detector
+face_cascade=cv2.CascadeClassifier("alg.xml")
+ds_factor=0.6
+class VideoCamera(object):
+    def __init__(self):
+       #capturing video
+       self.video = cv2.VideoCapture("y2mate.com - How to Start a Robot Revolution - Trailer_5hjlyA2eE9A_1080p.mp4")
+    def __del__(self):
+        #releasing camera
+        self.video.release()
+    @jit(target ="cuda")
+    def get_frame(self):
+       #extracting frames
+        ret, frame = self.video.read()
+        frame=cv2.resize(frame,None,fx=ds_factor,fy=ds_factor,
+        interpolation=cv2.INTER_AREA)                    
+        gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+        face_rects=face_cascade.detectMultiScale(gray,1.3,5)
+        for (x,y,w,h) in face_rects:
+         cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+         break
+        # encode OpenCV raw frame to jpg and displaying it
+        ret, jpeg = cv2.imencode('.jpg', frame)
+        return jpeg.tobytes()
