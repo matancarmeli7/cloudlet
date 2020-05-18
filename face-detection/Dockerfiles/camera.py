bbf977f5ceea2a8965c138c8329e66fba90a4090
Almog Golbar <57751689+almoggo@users.noreply.github.com>
2020-05-18 22:29:41 +0300

    Update camera.py

--- face-detection/Dockerfiles/camera.py
+++ face-detection/Dockerfiles/camera.py
@@ -12,7 +12,8 @@ class VideoCamera(object):
     def __del__(self):
         #releasing camera
         self.video.release()
-    @jit(target ="cuda")
+    @jit
+    #(target ="cuda")
     def get_frame(self):
        #extracting frames
         ret, frame = self.video.read()
