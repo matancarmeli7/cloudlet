fe2381d2099f727eacb9cfada5eb8d7190b8d045
Almog Golbar <57751689+almoggo@users.noreply.github.com>
2020-05-21 14:00:25 +0300

    Update camera.py

--- face-detection/Dockerfiles/camera.py
+++ face-detection/Dockerfiles/camera.py
@@ -15,7 +15,7 @@ class VideoCamera(object):
     def get_frame(self):
        #extracting frames
         ret, frame = self.video.read()
-        frame=cv2.UMat(frame)
+        #frame=cv2.UMat(frame)
         frame=cv2.resize(frame,None,fx=ds_factor,fy=ds_factor,
         interpolation=cv2.INTER_AREA)     
         gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
