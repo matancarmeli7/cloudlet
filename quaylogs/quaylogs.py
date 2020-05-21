5a4a8b8b43205d8e56713cceffee9b5cc7722260
meshistern <64001068+meshistern@users.noreply.github.com>
2020-05-21 11:33:20 +0300

    Update quaylogs.py

--- quaylogs/quaylogs.py
+++ quaylogs/quaylogs.py
@@ -4,17 +4,17 @@ import requests
 import schedule
 import time
 def gatherdata():
-    url = 'https://quayecosystem-quay-quay-enterprise.apps.ocp43-prod.sales.lab.tlv.redhat.com/api/v1/superuser/logs'
-    token = 'ieeVPVGwUsC8YVJAcM9ECA9zBn4SeRCPoYogmDet'
+    url = 'quay_url'
+    token = 'quay_token'
     hed = {'Authorization': 'Bearer ' + token}
     x = requests.get(url, headers=hed, verify=False)
     return(x.json())
 
 def senddata():
     mydata = gatherdata()
-    url = 'https://13.90.23.80:8088/services/collector/event'
-    myobj = {"event": {"event_type": "quaylogs", "cluster_name": "offline", "data": mydata }}
-    x = requests.post(url, json = myobj, auth=('Splunk', '6e5af836-9581-45b8-bcab-820bd9f7398e'), verify=False)
+    url = 'splunk_url'
+    myobj = {"event": {"event_type": "quaylogs", "cluster_name": "cluster_name", "data": mydata }}
+    x = requests.post(url, json = myobj, auth=('Splunk', 'splunk_token'), verify=False)
 schedule.every(3).minutes.do(senddata)
 while True:
     schedule.run_pending()
