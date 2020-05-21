21a6ceba6c57a523c7f98d47128a0e0597fb5e62
Almog Golbar <57751689+almoggo@users.noreply.github.com>
2020-05-21 10:23:17 +0300

    Update camera.py

--- face-detection/Dockerfiles/camera.py
+++ face-detection/Dockerfiles/camera.py
@@ -16,9 +16,9 @@ class VideoCamera(object):
        #extracting frames
         ret, frame = self.video.read()
         frame=cv2.resize(frame,None,fx=ds_factor,fy=ds_factor,
-        interpolation=cv2.INTER_AREA)                    
-        gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+        interpolation=cv2.INTER_AREA)     
         cv2.ocl.setUseOpenCL(True)
+        gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
         face_rects=face_cascade.detectMultiScale(gray,1.3,5)
         for (x,y,w,h) in face_rects:
          cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
