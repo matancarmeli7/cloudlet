936ef64f4fd2d3c2cb0426832a346f92b98c4532
root <root@sender.sales.lab.tlv.redhat.com>
2020-05-27 10:00:51 +0300

    quay logs update

--- quaylogs/quaylogs.py
+++ quaylogs/quaylogs.py
@@ -3,18 +3,26 @@
 import requests
 import schedule
 import time
-def gatherdata():
-    url = 'quay_url'
+def repolist():
+    url = 'quay_repo'
     token = 'quay_token'
     hed = {'Authorization': 'Bearer ' + token}
     x = requests.get(url, headers=hed, verify=False)
-    return(x.json())
-
+    return(x.json())	
+	
+	
 def senddata():
-    mydata = gatherdata()
-    url = 'splunk_url'
-    myobj = {"event": {"event_type": "quaylogs", "cluster_name": "cluster_name", "data": mydata }}
-    x = requests.post(url, json = myobj, auth=('Splunk', 'splunk_token'), verify=False)
+    myrepo = repolist()
+    splunk_url = 'splunk_event_url'
+    for repo in myrepo['results']:
+        reponame = repo['href']
+        print (reponame)
+        url = quay_mirror
+        token = 'quay_token'
+        hed = {'Authorization': 'Bearer ' + token}
+        mydata = requests.get(url, headers=hed, verify=False)
+        myobj = {"event":{"event_type": "quaylogs", "cluster": "prod", "organization": repo['namespace']['name'],"repository": repo['name'],"data": mydata.json() }}
+        r = requests.post(splunk_url, json = myobj, auth=('Splunk', '6e5af836-9581-45b8-bcab-820bd9f7398e'), verify=False)
 schedule.every(3).minutes.do(senddata)
 while True:
     schedule.run_pending()
