955042ea8ee7a353596687489c096aba6e8fddbb
snirmusayof <63164964+snirmusayof@users.noreply.github.com>
2020-05-27 10:39:19 +0300

    Update camera.py

--- face-detection/Dockerfiles/camera.py
+++ face-detection/Dockerfiles/camera.py
@@ -28,3 +28,21 @@ class VideoCamera(object):
         frame =  cv2.UMat.get(frame)
         ret, jpeg = cv2.imencode('.jpg', frame)
         return jpeg.tobytes()
+    
+    def frame(self):
+       #extracting frames
+        ret, frame = self.video.read()
+        frame=cv2(frame)
+        frame=cv2.resize(frame,None,fx=ds_factor,fy=ds_factor,
+        interpolation=cv2.INTER_AREA)     
+        gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+        gray=cv2(gray)
+       cv2.ocl.setUseOpenCL(False)
+        face_rects=face_cascade.detectMultiScale(gray,1.3,5)
+        for (x,y,w,h) in face_rects:
+         cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+         break
+        # encode OpenCV raw frame to jpg and displaying it
+        frame =  cv2.get(frame)
+        ret, jpeg = cv2.imencode('.jpg', frame)
+        return jpeg.tobytes()
