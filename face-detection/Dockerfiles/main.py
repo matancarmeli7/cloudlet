936622b8b663d2932c188faa5b733b87a73f25f9
snirmusayof <63164964+snirmusayof@users.noreply.github.com>
2020-06-01 09:58:36 +0300

    Update main.py

--- face-detection/Dockerfiles/main.py
+++ face-detection/Dockerfiles/main.py
@@ -2,6 +2,9 @@
 # import the necessary packages
 from flask import Flask, render_template, Response
 from camera import VideoCamera
+import cv2
+from timeit import default_timer as timer
+
 app = Flask(__name__)
 @app.route('/')
 def index():
@@ -9,8 +12,9 @@ def index():
     return render_template('index.html')
 def gen(camera):
     while True:
-        #get camera frame
+        start = timer()
         frame = camera.get_frame_gpu()
+        print("with GPU", timer()-start)
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 @app.route('/video_feed')
