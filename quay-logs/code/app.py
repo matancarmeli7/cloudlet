d68356a68c206a6c3c10af81459c50161cdcdd39
root <root@ocp4-dev-installer.4upqoasn12zunbwqjgn0yadptc.bx.internal.cloudapp.net>
2020-09-16 07:16:49 +0000

    update quay-logs

--- quay-logs/code/app.py
+++ quay-logs/code/app.py
@@ -26,7 +26,6 @@ def senddata():
     splunk_url = splunk_event_url
     for repo in myrepo['results']:
         reponame = repo['href']
-        print(reponame)
         url = quay_repo + '/api/v1' + reponame
         token = quay_token
         hed = {'Authorization': 'Bearer ' + token}
@@ -35,15 +34,12 @@ def senddata():
         my_image_json = my_image.json()
         my_tags_json = my_tags.json()
         for data in my_tags_json['tags']:
-            print(my_image_json)
             image = my_image_json['external_reference']
-            print(image)
             regex = re.search('[^\/]+$', image)
             myobj = {"event":{"event_type": "quaylogs", "cluster": cluster_name, "organization": repo['namespace']['name'],"repository": repo['name'], "image": regex.group(), "image_tag": data, "data": my_image_json }}
             r = requests.post(splunk_url, json = myobj, auth=('Splunk', splunk_token), verify=False)
             print(r)
 
-#schedule.every(3).minutes.do(senddata)
-#while True:
-#    schedule.run_pending()
-senddata()
+schedule.every(3).minutes.do(senddata)
+while True:
+    schedule.run_pending()
--- quay-logs/code/config.py
+++ quay-logs/code/config.py
@@ -1,5 +1,5 @@
-quay_repo = 'https://quayecosystem-quay-quay-enterprise.apps.ocp43-dev.cloudlet-dev.com'
+quay_repo = 'https://quayecosystem.apps.ocp43-dev.cloudlet-dev.com'
 splunk_event_url = 'https://13.90.23.80:8088/services/collector/event'
-cluster_name= 'ocp43-dev'
-quay_token = 'q6bmh365EpTHQyrqBcCnqVicIi1er7LLNdK2q4NV'
+cluster_name= 'ocp43-np'
+quay_token = 'm22zF0WNdB0apZXsURx5YNvyukGoaeMefCziaRWp'
 splunk_token = '76dfd089-67f2-4262-978e-755af5c0775a'
