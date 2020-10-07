55ab83675ee49448afdfd1718ec20e9964a20e46
Administrator <admin@example.com>
2020-10-07 07:23:28 +0000

    Update minio-test

--- minio-app-test/app.py
+++ minio-app-test/app.py
@@ -3,11 +3,25 @@ import time
 from minio import Minio
 from minio.error import ResponseError
 import sys
+import urllib3
+
+urllib3.disable_warnings(urllib3.exceptions.SecurityWarning)
 
 sys.path.insert(1, '/minio-test/src/config')
 from config import *
+
+httpClient = urllib3.PoolManager(
+                timeout=urllib3.Timeout.DEFAULT_TIMEOUT,
+                        cert_reqs='CERT_REQUIRED',
+                        ca_certs='/etc/pki/ca-trust/source/anchors/rootCA.pem',
+                        retries=urllib3.Retry(
+                            total=5,
+                            backoff_factor=0.2,
+                            status_forcelist=[500, 502, 503, 504]))
+
 minioClient = Minio(minio_url,
                     secure = False,
+                    http_client=httpClient,
                     access_key = minio_access_key,
                     secret_key = minio_secret_key)
 
--- minio-app-test/config.py
+++ minio-app-test/config.py
@@ -1,4 +1,4 @@
 bucket_name = 'mybucket'
-minio_url = 'minio-route-default.apps.ocp43-dev.cloudlet-dev.com'
+minio_url = 'minio.apps.dev.cloudlet-dev.com'
 minio_access_key = 'minio'
 minio_secret_key = 'minio123'
