f104af15906fe9b91ea73602bbbaa65d2c5a06d1
Administrator <admin@example.com>
2020-10-29 07:34:42 +0000

    update quaylogs and argo-to-splunk

--- quay-logs/code/app.py
+++ quay-logs/code/app.py
@@ -1,34 +1,44 @@
 import requests
-import schedule
 import time
 import urllib3
 import json
 import re
 import sys
+import base64
+from kubernetes import client, config
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-sys.path.insert(1, '/quay-logs/src/config')
+sys.path.insert(1, '/quaylogs/src/config')
 from config import *
 
-def repolist():
+def quayToken():
+    config.load_kube_config('kube')
+    v1 = client.CoreV1Api()
+    base64_data_json=v1.read_namespaced_secret('quay-token-secret', 'quay-enterprise')
+    base64_data = base64_data_json.data['token']
+    base64_bytes = base64_data.encode('ascii')
+    message_bytes = base64.b64decode(base64_bytes)
+    data_decode = message_bytes.decode('ascii')
+    return(data_decode)
+
+def repolist(quay_token):
 
     url = quay_repo + '/api/v1/find/repositories'
-    token = quay_token
-    hed = {'Authorization': 'Bearer ' + token}
+    hed = {'Authorization': 'Bearer ' + quay_token}
     x = requests.get(url, headers=hed, verify=False)
     return(x.json())
 
 
 def senddata():
-
-    myrepo = repolist()
+    
+    quay_token = quayToken()
+    myrepo = repolist(quay_token)
     splunk_url = splunk_event_url
     for repo in myrepo['results']:
         reponame = repo['href']
         url = quay_repo + '/api/v1' + reponame
-        token = quay_token
-        hed = {'Authorization': 'Bearer ' + token}
+        hed = {'Authorization': 'Bearer ' + quay_token}
         my_tags =  requests.get(url, headers=hed, verify=False)
         my_image =  requests.get(url + '/mirror', headers=hed, verify=False)
         my_image_json = my_image.json()
@@ -40,6 +50,4 @@ def senddata():
             r = requests.post(splunk_url, json = myobj, auth=('Splunk', splunk_token), verify=False)
             print(r)
 
-schedule.every(3).minutes.do(senddata)
-while True:
-    schedule.run_pending()
+senddata()
---
+++ quay-logs/code/start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export KUBECONFIG=./kubeconfig
+oc login https://api."$fqdn":6443 -u admin -p redhat --insecure-skip-tls-verify=true > /dev/null 2>&1
+oc config view > kube
